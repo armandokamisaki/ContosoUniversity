900e2b80c4ef3e54c842ec181bd965cdd6e44ec1
armando kamisaki <akamisaki@gmail.com>
2023-06-23 19:13:36 -0300

    Add project files.

---
+++ Migrations/20230622013525_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+public partial class InitialCreate : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "Course",
+            columns: table => new
+            {
+                CourseID = table.Column<int>(nullable: false),
+                Credits = table.Column<int>(nullable: false),
+                Title = table.Column<string>(nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Course", x => x.CourseID);
+            });
+
+        // Additional code not shown
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Enrollment");
+        // Additional code not shown
+    }
+}
